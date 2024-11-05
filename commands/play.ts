37bb6aedd39eaeaa533ba36c1c6234c86d33f943
Mads Hauberg <mail@hotbear.org>
2024-11-06 00:50:02 +0100

    Update play.ts

--- commands/play.ts
+++ commands/play.ts
@@ -49,5 +49,7 @@ export default async function play(message: Message<boolean>, song: types.jsonSo
         return currentQueue.textChannel.send(`🎶 | Added **${track.tracks[0].title}** to the queue [${currentQueue.queue.getSize()}]`);;
     }
 
-    currentQueue.audioPlayer.play(currentQueue.voiceChannel, track);
+    currentQueue.audioPlayer.extractors.loadDefault();
+
+    currentQueue.audioPlayer.play(currentQueue.voiceChannel, song.url);
 }
